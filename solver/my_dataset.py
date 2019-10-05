f57b65ee5630d3492d12b9435fdfbd03fce9b95b
Tony Liu <ipwnu777@gmail.com>
2019-10-05 18:18:49 +1300

    Fixed imports

--- solver/my_dataset.py
+++ solver/my_dataset.py
@@ -4,7 +4,7 @@ import os
 import torchvision.transforms as transforms
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
-from solver import captcha_setting
+from solver import captcha_setting, one_hot_encoding as ohe
 
 
 class mydataset(Dataset):
