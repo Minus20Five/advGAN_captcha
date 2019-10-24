8c191279e0b86e90e95193a87c7e4c99eeaac48a
NotWowe <henryli333@gmail.com>
2019-10-25 00:11:42 +1300

    Changed graphing data for new data

--- graphing/graph_adv_training_accuracies.py
+++ graphing/graph_adv_training_accuracies.py
@@ -3,37 +3,161 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
  
+
+standard_normal = [
+            0.125,
+            0.409375,
+            0.6109375,
+            0.6296875,
+            0.5921875,
+            0.7640625,
+            0.6734375,
+            0.8890625,
+            0.8890625,
+            0.840625,
+            0.909375,
+            0.9359375,
+            0.93125,
+            0.9359375,
+            0.9171875,
+            0.8484375,
+            0.9625,
+            0.8921875,
+            0.9265625,
+            0.909375,
+            0.928125,
+            0.878125,
+            0.86875,
+            0.63125,
+            0.9671875,
+            0.9125,
+            0.8046875,
+            0.946875
+        ]
+standard_adv = [
+            0.111979167,
+            0.09765625,
+            0.153645833,
+            0.182291667,
+            0.19140625,
+            0.1796875,
+            0.1796875,
+            0.28515625,
+            0.325520833,
+            0.20703125,
+            0.279947917,
+            0.2421875,
+            0.24609375,
+            0.305989583,
+            0.274739583,
+            0.291666667,
+            0.397135417,
+            0.27734375,
+            0.256510417,
+            0.321614583,
+            0.302083333,
+            0.240885417,
+            0.299479167,
+            0.264322917,
+            0.388020833,
+            0.381510417,
+            0.251302083,
+            0.259114583
+        ]
+advtrain_normal = [
+            0.1046875,
+            0.765625,
+            0.215625,
+            0.8703125,
+            0.934375,
+            0.9484375,
+            0.9484375,
+            0.975,
+            0.9671875,
+            0.9328125,
+            0.9609375,
+            0.9109375,
+            0.95,
+            0.9609375,
+            0.9453125,
+            0.690625,
+            0.9859375,
+            0.98125,
+            0.98125,
+            0.940625,
+            0.971875,
+            0.8296875,
+            0.9,
+            0.95,
+            0.975,
+            0.94,
+            0.96,
+            0.95
+        ]
+advtrain_adv = [
+            0.0625,
+            0.4125,
+            0.121875,
+            0.7,
+            0.7953125,
+            0.7828125,
+            0.8703125,
+            0.690625,
+            0.7359375,
+            0.553125,
+            0.8609375,
+            0.671875,
+            0.5421875,
+            0.809375,
+            0.65,
+            0.78125,
+            0.9296875,
+            0.85,
+            0.9296875,
+            0.609375,
+            0.8140625,
+            0.4359375,
+            0.65,
+            0.87,
+            0.93,
+            0.77,
+            0.8,
+            0.83
+        ]
+
 # Data
 df=pd.DataFrame(
     {
-        'epoch': range(1,22),
-        'weighted-normal': [4.371316306, 85.75638507, 36.2475442, 78.58546169, 84.13555992, 96.80746562, 60.31434185, 51.66994106, 45.23575639, 26.22789784, 86.64047151, 55.35363458, 39.04715128, 83.93909627, 47.8388998, 56.72888016, 43.86051081, 41.35559921, 49.45972495, 12.62278978, 65.32416503],
-        'weighted-adv': [10.65815324, 90.86444008, 68.32023576, 84.03732809, 54.12573674, 96.26719057, 82.66208251, 90.5697446, 51.27701375, 55.35363458, 94.79371316, 80.74656189, 68.95874263, 94.10609037, 83.74263261, 88.4086444, 78.92927308, 59.67583497, 67.14145383, 17.23968566, 92.82907662], 
-        'unweighted-normal': [51.2278978, 66.8467583, 80.697446, 66.9449902, 53.0451866, 85.21611, 93.762279, 97.3477407, 83.4479371, 75.9332024, 65.2750491, 60.7072692, 79.9115914, 97.3477407, 58.7917485, 59.7249509, 80.8939096, 88.8998035, 55.6483301, 97.2003929, 60.3143418],
-        'unweighted-adv': [65.2750491, 81.4341847, 93.8605108, 96.4145383, 90.8644401, 98.0353635, 96.611002, 94.9410609, 97.740668, 96.3163065, 92.4361493, 91.7976424, 95.1866405, 98.3791749, 88.5559921, 82.2691552, 91.9449902, 95.6777996, 83.5952849, 98.280943, 84.4302554]
+        'epoch': range(1,29),
+        'standard-normal': [i * 100 for i in standard_normal],
+        'standard-adv': [i * 100 for i in standard_adv],
+        'advtrain-normal': [i * 100 for i in advtrain_normal],
+        'advtrain-adv': [i * 100 for i in advtrain_adv]
     }
 )
  
-plt.title('Unweighted Adversarially Trained Solver Accuracy Per Epoch')
-plt.plot( 'epoch', 'unweighted-normal', data=df, marker='', linewidth=2, label='Normal')
-plt.plot( 'epoch', 'unweighted-adv', data=df, marker='', linewidth=2, label='Adversarial')
+plt.title('Normally Trained Solver Accuracy Per Epoch')
+plt.plot( 'epoch', 'standard-normal', data=df, marker='', linewidth=2, label='Normal')
+plt.plot( 'epoch', 'standard-adv', data=df, marker='', linewidth=2, label='Adversarial')
 
 plt.xlabel('Epoch Number')
-plt.xticks(range(1,22))
-plt.ylabel('Accuracy (%%)')
+plt.xticks(range(1,29))
+plt.ylabel('Accuracy (%)')
+plt.ylim(0,100)
 plt.legend()
 
 plt.show()
 
 plt.clf()
 
-plt.title('Weighted Adversarially Trained Solver Accuracy Per Epoch')
-plt.plot( 'epoch', 'weighted-normal', data=df, marker='', linewidth=2, label='Normal')
-plt.plot( 'epoch', 'weighted-adv', data=df, marker='', linewidth=2, label='Adversarial')
+plt.title('Adversarially Trained Solver Accuracy Per Epoch')
+plt.plot( 'epoch', 'advtrain-normal', data=df, marker='', linewidth=2, label='Normal')
+plt.plot( 'epoch', 'advtrain-adv', data=df, marker='', linewidth=2, label='Adversarial')
 
 plt.xlabel('Epoch Number')
-plt.xticks(range(1,22))
-plt.ylabel('Accuracy (%%)')
+plt.xticks(range(1,29))
+plt.ylabel('Accuracy (%)')
+plt.ylim(0,100)
 plt.legend()
 
 plt.show()
